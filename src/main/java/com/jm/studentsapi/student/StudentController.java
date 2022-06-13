3a675a31315343cd6ca3065c7064d807845e14b1
Jocyline Magero <jmagero54@gmail.com>
2022-06-13 16:30:18 -0500

    Add student controller

--- src/main/java/com/jm/studentsapi/student/StudentController.java
+++ src/main/java/com/jm/studentsapi/student/StudentController.java
@@ -3,11 +3,14 @@ package com.jm.studentsapi.student;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -27,9 +30,18 @@ public class StudentController {
     	studentService.addStudent(student);	
     }
     
-    public void updateStudent(@RequestBody Student student, @PathVariable Long id) {
-    	studentService.updateStudent(student);
+    @PutMapping(path="{id}")
+    public void updateStudent(
+    		@PathVariable Long id,
+    		@RequestParam(required = false) String email,
+    		@RequestParam(required = false) String name
+    		) {
+    	studentService.updateStudent(id,email, name);
     }
     
+    @DeleteMapping(path="{id}")
+    public void deleteStudent(@PathVariable Long id){
+    	 studentService.deleteStudent(id);
+    }
 
 }

1265e2cf07eee33e326b2944a82ea2a28c56ca70
Jocyline Magero <jmagero54@gmail.com>
2022-06-13 16:30:36 -0500

    remove comments

--- src/main/java/com/jm/studentsapi/StudentsApiApplication.java
+++ src/main/java/com/jm/studentsapi/StudentsApiApplication.java
@@ -3,11 +3,24 @@ package com.jm.studentsapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
+//@RestController
 public class StudentsApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StudentsApiApplication.class, args);
 	}
-
+	
+//	@GetMapping
+//	public List<Student> helo() {
+//		return List.of(new Student(
+//				1,
+//				"Mariam",
+//				"mariam@gmail.ome",
+//				LocalDate.of(2000, 01,20),
+//				21));
+//	}
+	
+	
 }

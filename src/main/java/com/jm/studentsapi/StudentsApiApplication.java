381cb9ab566fb8b31d9c3519788ca9e4d906ebd9
Jocyline Magero <jmagero54@gmail.com>
2022-06-24 14:48:51 -0500

    setup project for deploy

--- src/main/java/com/jm/studentsapi/StudentsApiApplication.java
+++ src/main/java/com/jm/studentsapi/StudentsApiApplication.java
@@ -2,25 +2,15 @@ package com.jm.studentsapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 
 @SpringBootApplication
-//@RestController
+@PropertySource("classpath:application-${spring.profiles.active:default}.properties")
 public class StudentsApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StudentsApiApplication.class, args);
 	}
-	
-//	@GetMapping
-//	public List<Student> helo() {
-//		return List.of(new Student(
-//				1,
-//				"Mariam",
-//				"mariam@gmail.ome",
-//				LocalDate.of(2000, 01,20),
-//				21));
-//	}
-	
 	
 }
